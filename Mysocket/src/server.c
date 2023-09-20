eda6d163090f332106d965a54167af1fac7e2aef
Narek <124366382+Nar2005ek@users.noreply.github.com>
2023-09-20 15:47:47 +0400

    Update server.c

--- Mysocket/src/server.c
+++ Mysocket/src/server.c
@@ -4,14 +4,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-void* handleUser(void* sockPtr) {
-    const int bufferSize = 1024;
+void* handleUser(void* sockPtr) 
+{
+    const int bufferSize = 16384;
     int userSocket = *((int*)sockPtr);
     free(sockPtr);
 
     char buffer[bufferSize];
-    ssize_t recivedBytes;
+    ssize_t recivedBytes; 
     while(1) {
         recivedBytes = recv(userSocket, buffer, bufferSize, 0);
         if(recivedBytes <= 0) {
@@ -23,14 +25,53 @@ void* handleUser(void* sockPtr) {
         if(strncmp(buffer, "disconnect", 10) == 0) {
             printf("Client requested disconnect.\n");
             break;
-        } else if(strncmp(buffer, "shell ", 6) == 0) {
+        } 
+        else if(strncmp(buffer, "shell ", 6) == 0) {
             char *command = buffer + 6;
-            printf("Recived shell command: %s\n", command);
+            int count = 0;
+            while(command[count] == ' ') {
+                ++count;
+            }
+            command = buffer + count + 6;
+            if(strncmp(command, "vim", 3) == 0 || strncmp(command, "dd", 2) == 0 || strncmp(command, "man", 3) == 0 || strncmp(command, "top", 3) == 0 || strncmp(command, "nano", 4) == 0 || strncmp(command, "less", 4) == 0 || strncmp(command, "passwd", 6) == 0){
+                printf("Recived shell command: %s", command);
+                strcpy(buffer, "Error: Unable to execute shell command.");
+                send(userSocket, buffer, strlen(buffer), 0);
+                continue;
+            } 
+            else if(strncmp(command, "sudo", 4) == 0) {
+                strcpy(buffer, "Error: Can't do operations with sudo.");
+                send(userSocket, buffer, strlen(buffer), 0);
+                continue;
+            } 
+            else if(strncmp(command, "cat", 3) == 0) {
+                char* fileName = command + 3;
+                count = 0;
+                bool flag = false;
+                while(fileName[count] != '\0') {
+                    if(fileName[count] != 32 && fileName[count] != 10) {
+                        flag = true;
+                        break;
+                    }
+                    ++count;
+                }
+                if(flag) {
+                    fileName = command + count + 3;
+                } 
+                else {
+                    printf("Recived shell command: %s", command);
+                    strcpy(buffer, "Error: Unable to execute shell command.");
+                    send(userSocket, buffer, strlen(buffer), 0);
+                    continue;
+                }
+            }
+            printf("Recived shell command: %s", command);
             FILE* filePtr = popen(command, "r");
             if(filePtr == NULL) {
                 perror("popen");
                 strcpy(buffer, "Error: Unable to execute shell command.");
-            } else {
+            }
+            else {
                 char answer[bufferSize];
                 memset(answer, 0, sizeof(answer));
                 char line[bufferSize];
@@ -41,7 +82,8 @@ void* handleUser(void* sockPtr) {
                 }
                 if(count == 0) {
                     strcpy(buffer, "Error: Unable to execute shell command.");
-                } else {
+                }
+                else {
                     pclose(filePtr);
                     strcpy(buffer, answer);
                 }
@@ -54,15 +96,15 @@ void* handleUser(void* sockPtr) {
     return NULL;
 }
 
-int main() {
+int main() 
+{
     const int maxClients = 5;
-    int serverSocket;
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     int userSocket;
     struct sockaddr_in serverAddr;
     struct sockaddr_in userAddr;
     socklen_t userLen = sizeof(userAddr);
 
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if(serverSocket == -1) {
         perror("socket");
         return 1;
